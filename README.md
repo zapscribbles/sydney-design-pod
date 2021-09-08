7c36c98109b07c36db5fdff4acbdbdaae16d3194
scribblerp9 <szappia86@gmail.com>
2021-09-08 16:55:27 +1000

    Documentation
    Updated readme and added documentation to contentEditor.js

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# 11ty-boilerplate
+# Sydney Design Pod
 
-My own little 11ty starter project. Based off https://css-tricks.com/eleventy-starter-with-tailwind-css-alpine-js/
+## Portfolio site
 
-Includes:
-- Tailwind CSS
-- AlpineJS
-- Uses Nunjucks for layouts and partials, HTML for pages
-- Netlify-ready
-- HTML and CSS is minified on build, and _site is deleted
+To run local dev server, first install Netlify CLI globally:
+`npm i -g netlify-cli`
+
+Then run
+`ntl dev`
+
+Tech stack:
+
+-   Eleventy
+-   Tailwind CSS
+-   AlpineJS
+-   Uses Nunjucks for layouts and partials
+-   Custom CMS
+
+The end user wanted a simple way to provide content changes, so I wrote a little script that does just that! Check it out [here](src/common-js/contentEditor.js)
+
+Questions?
+Email me at stephen.zappia@gmail.com
--- src/common-js/contentEditor.js
+++ src/common-js/contentEditor.js
@@ -1,3 +1,19 @@
+/* Stephen's Simple CMS
+    This is about as simple as it gets!
+    This is useful for initial builds or brand new pages - for ongoing content management, something like Netlify CMS would be more appropriate.
+
+    Here's how it works:
+    1) In code, add the contenteditable attribute to any HTML elements you want to be editable. N.B. I don't recommend using it on interactive elements like buttons
+    2) End user will then be able to edit that content when they're viewing the site
+    3) Changes they make to content are saved in their browser's localStorage, so do not that this is device and browser specific!
+    4) They can then click "Copy All Changes" in the menu to copy all changed content fields from localStorage to their clipboard in JSON format
+    5) This can then be pasted into an email to the developer to update content accordingly
+
+    TODO: Send JSON data automatically, either opening a new email, or even better via an API/using Zapier
+    TODO: Better way of ensuring fields (and therefore content) are consistent across build versions, so if the end user is editing a live version but the dev is working on a local version/different branch they will still be able to match the content data they receive to the right fields
+    TODO: Automatically update the content and save it to code without any dev interaction. This is the dream.
+*/
+
 // Prepare some helper data for identifying elements etc
 var buildVersion = document.currentScript.getAttribute('buildVersion');
 var urlPathArray = window.location.pathname.split('/');
